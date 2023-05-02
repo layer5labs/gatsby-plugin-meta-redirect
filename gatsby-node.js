78bc1b97921b9e8099f3faced8eca65d0fe461d0
Randy Lau <randychilau@gmail.com>
2023-05-02 13:53:23 -0700

    adding edits customized for GitHub Pages
    Signed-off-by: Randy Lau <randychilau@gmail.com>

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,7 +3,8 @@ const { exists, writeFile, ensureDir } = require('fs-extra');
 
 const getMetaRedirect = require('./getMetaRedirect');
 
-async function writeRedirectsFile(redirects, folder, pathPrefix) {
+async function writeRedirectsFile(redirects, folder, pathPrefix, siteUrl) {
+
   if (!redirects.length) return;
 
   for (const redirect of redirects) {
@@ -11,8 +12,8 @@ async function writeRedirectsFile(redirects, folder, pathPrefix) {
 
     const FILE_PATH = path.join(
       folder,
-      fromPath.replace(pathPrefix, ''),
-      'index.html'
+      fromPath.replace(pathPrefix, ""),
+      "index.html"
     );
 
     const fileExists = await exists(FILE_PATH);
@@ -23,7 +24,7 @@ async function writeRedirectsFile(redirects, folder, pathPrefix) {
         // ignore if the directory already exists;
       }
 
-      const data = getMetaRedirect(toPath);
+      const data = getMetaRedirect(toPath, pathPrefix, siteUrl);
       await writeFile(FILE_PATH, data);
     }
   }
@@ -32,11 +33,36 @@ async function writeRedirectsFile(redirects, folder, pathPrefix) {
 exports.onPostBuild = ({ store }) => {
   const { redirects, program, config } = store.getState();
 
-  let pathPrefix = '';
+  let pathPrefix = "";
   if (program.prefixPaths) {
     pathPrefix = config.pathPrefix;
   }
 
-  const folder = path.join(program.directory, 'public');
-  return writeRedirectsFile(redirects, folder, pathPrefix);
+  const siteUrl = config.siteMetadata.siteUrl;
+  const folder = path.join(program.directory, "public");
+
+  return writeRedirectsFile(redirects, folder, pathPrefix, siteUrl);
 };
+
+/* MIT License
+
+Copyright (c) 2018 Get Chalk
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
--- getMetaRedirect.js
+++ getMetaRedirect.js
@@ -1,18 +1,57 @@
-module.exports = function getMetaRedirect(toPath) {
-  let url = toPath.trim();
+module.exports = function getMetaRedirect(toPath, pathPrefix, siteUrl) {
 
-  const hasProtocol = url.includes('://');
+  const ci = process.env.CI;
+
+  let url = toPath.replace(pathPrefix, "").trim();
+
+  const hasProtocol = url.includes("://");
   if (!hasProtocol) {
-    const hasLeadingSlash = url.startsWith('/');
+
+    const hasLeadingSlash = url.startsWith("/");
     if (!hasLeadingSlash) {
       url = `/${url}`;
     }
 
-    const resemblesFile = url.includes('.');
+    const resemblesFile = url.includes(".");
     if (!resemblesFile) {
-      url = `${url}/`.replace(/\/\/+/g, '/');
+    /*modified for GitHub Pages due to url handling, more info
+    https://slorber.github.io/trailing-slash-guide/.
+    original code:
+    url = `${url}/`.replace(/\/\/+/g, '/');
+    */
+      url = ci
+        ? url = `${url}`.replace(/\/\/+/g, "/")
+        : url = `${url}/`.replace(/\/\/+/g, "/");
     }
   }
 
-  return `<meta http-equiv="refresh" content="0; URL='${url}'" />`;
+  const metaRefresh = `<meta http-equiv="refresh" content="0; URL='${pathPrefix}${url}'" />`;
+
+  const metaCanonical =  `<link rel="canonical" href="${siteUrl}${pathPrefix}${url}" />`;
+
+  return metaRefresh + metaCanonical;
 };
+
+
+/* MIT License
+
+Copyright (c) 2018 Get Chalk
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
